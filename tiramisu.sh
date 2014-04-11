93e9f3edb9ca0558050106942ddc80a6622e9a0f
Tim Clem <tim.clem@gmail.com>
2014-04-11 13:23:35 -0400

    Replace company name and bundle domain via tiramisu

--- tiramisu.sh
+++ tiramisu.sh
@@ -5,9 +5,19 @@ BOOTSTRAP_REPO="git://github.com/crushlovely/Amaro.git"
 BOOTSTRAP_WEBSITE="https://github.com/crushlovely/Amaro"
 BOOTSTRAP_BRANCH=master
 
-DEFAULT_PREFIX=CRBS
-DEFAULT_PROJECT_NAME=CrushBootstrap
+# These are values that are replaced by user input
+BOOTSTRAP_PREFIX=CRBS
+BOOTSTRAP_PROJECT_NAME=CrushBootstrap
+BOOTSTRAP_ORG="Crush & Lovely"
+BOOTSTRAP_DOMAIN="com.crushlovely"
+
+
 DEFAULT_FULLNAME=$(dscl . read /Users/`whoami` RealName | sed -n $'s/^[\t ]*//;2p')
+
+set +e
+DEFAULT_ORG=$(/usr/libexec/PlistBuddy -c 'Print :IDETemplateOptions:organizationName' "${HOME}/Library/Preferences/com.apple.dt.Xcode.plist")
+set -e
+[[ -z "$DEFAULT_ORG" ]] && DEFAULT_ORG="MyCompany"
 
 
 
@@ -178,7 +188,7 @@ PROJECT_NAME=$(trim "$ORIG_PROJECT_NAME" | tr -s ' ' | tr ' ' '-')
 [[ $(dirname -- "$PROJECT_NAME") != "." ]] && die "No paths in your name, silly!"
 [[ "$PROJECT_NAME" != "$ORIG_PROJECT_NAME" ]] && echo " ✨  Fixed that for you. Using '$PROJECT_NAME'"
 [[ -e "$PROJECT_NAME" ]] && die "A file already exists with that name!"
-[[ "$PROJECT_NAME" == "$DEFAULT_PROJECT_NAME" ]] && die "Very funny."
+[[ "$PROJECT_NAME" == "$BOOTSTRAP_PROJECT_NAME" ]] && die "Very funny."
 
 
 ### Prefix
@@ -186,12 +196,12 @@ isBlacklistedPrefix() {
     # http://www.fvue.nl/wiki/Bash:_Check_if_array_element_exists
     # This is not a complete list, but certainly hits the highpoints
     BAD_PREFIXES=( AB AC AD AL AU AV CA CB CF CG CI CL CM CV EA EK GC JS MA MC MF MK NK NS PK QL SC SK SL SS TW UI UT )
-    
+
     local needle=$1
     shift
-    
+
     set -- ${BAD_PREFIXES[@]}
-    
+
     for prefix; do
         [[ $prefix == $needle ]] && return 0
     done
@@ -207,15 +217,35 @@ PREFIX=$(echo "$PREFIX" | tr '[:lower:]' '[:upper:]')
 [[ ${#PREFIX} > 3 ]] && die "Prefix is too long. Ain't nobody got time to type that."
 [[ $PREFIX =~ ^[A-Z][A-Z0-9]+$ ]] || die "Prefix is an invalid identifier"
 isBlacklistedPrefix "$PREFIX" && die "That prefix is already used by Apple"
-[[ "$PREFIX" == "$DEFAULT_PREFIX" ]] && die "Very funny."
+[[ "$PREFIX" == "$BOOTSTRAP_PREFIX" ]] && die "Very funny."
 
 
 ### Full name
 read -p "Your name (blank for $DEFAULT_FULLNAME): " FULLNAME
 FULLNAME=$(trim "$FULLNAME")
 if [[ -z "$FULLNAME" ]]; then
-    FULLNAME=$DEFAULT_FULLNAME
+    FULLNAME="$DEFAULT_FULLNAME"
     echo " ✨  Using name $FULLNAME"
+fi
+
+
+### Organization Name
+read -p "Organization name (blank for $DEFAULT_ORG): " ORG_NAME
+ORG_NAME=$(trim "$ORG_NAME")
+if [[ -z "$ORG_NAME" ]]; then
+    ORG_NAME="$DEFAULT_ORG"
+    echo " ✨  Using organization $DEFAULT_ORG"
+fi
+
+
+### Bundle ID domain
+DEFAULT_DOMAIN=$(echo "$ORG_NAME" | sed -e 's/[^[:alnum:]]//g' | tr A-Z a-z)
+DEFAULT_DOMAIN="com.$DEFAULT_DOMAIN"
+read -p "Bundle ID domain (blank for $DEFAULT_DOMAIN): " BUNDLE_DOMAIN
+BUNDLE_DOMAIN=$(trim "$BUNDLE_DOMAIN")
+if [[ -z "$BUNDLE_DOMAIN" ]]; then
+  BUNDLE_DOMAIN="$DEFAULT_DOMAIN"
+  echo " ✨  Using domain $DEFAULT_DOMAIN"
 fi
 
 
@@ -258,30 +288,30 @@ echo "👍"
 
 renameProjectFile() {
     OLD_NAME="$1"
-    NEW_NAME=$(echo "$1" | sed "s/$DEFAULT_PROJECT_NAME/$PROJECT_NAME/")
+    NEW_NAME=$(echo "$1" | sed "s/$BOOTSTRAP_PROJECT_NAME/$PROJECT_NAME/")
     git mv "$OLD_NAME" "$NEW_NAME"
 }
 
 renamePrefixedFile() {
     OLD_NAME="$1"
-    NEW_NAME=$(echo "$1" | sed "s/^$DEFAULT_PREFIX/$PREFIX/")
+    NEW_NAME=$(echo "$1" | sed "s/^$BOOTSTRAP_PREFIX/$PREFIX/")
     git mv "$OLD_NAME" "$NEW_NAME"
 }
 
 # Make these variables and functions available to our find -execs below
 export -f renameProjectFile
 export -f renamePrefixedFile
-export DEFAULT_PROJECT_NAME PROJECT_NAME DEFAULT_PREFIX PREFIX FULLNAME
+export BOOTSTRAP_PROJECT_NAME PROJECT_NAME BOOTSTRAP_PREFIX PREFIX FULLNAME
 
 echo -n "Renaming files... "
 
 # The -d is for a depth-first search, which ensures that files get renamed before their parent
 # directories, which would break things, obviously.
 # Re: the bash -c and $0 craziness, see http://stackoverflow.com/questions/4321456/find-exec-a-shell-function
-find . -d -name "*$DEFAULT_PROJECT_NAME*" \( -type f -o -type d \)  -not \( -path './.git/*' -prune \) -execdir bash -c 'renameProjectFile "$0"' {} \;
+find . -d -name "*$BOOTSTRAP_PROJECT_NAME*" \( -type f -o -type d \)  -not \( -path './.git/*' -prune \) -execdir bash -c 'renameProjectFile "$0"' {} \;
 
-if [[ "$PREFIX" != "$DEFAULT_PREFIX" ]]; then
-    find . -type f -name "$DEFAULT_PREFIX*" -not \( -path './.git/*' -prune \) -execdir bash -c 'renamePrefixedFile "$0"' {} \;
+if [[ "$PREFIX" != "$BOOTSTRAP_PREFIX" ]]; then
+    find . -type f -name "$BOOTSTRAP_PREFIX*" -not \( -path './.git/*' -prune \) -execdir bash -c 'renamePrefixedFile "$0"' {} \;
 fi
 
 echo "👍"
@@ -294,11 +324,15 @@ echo -n "Updating file contents... "
 switchToCLocale
 
 # Any reference to the project name or the prefix in all files:
-find . -type f -not \( -path './.git/*' -prune \) -not -path './README.md' -exec sed -i '' "s/$DEFAULT_PROJECT_NAME/$PROJECT_NAME/g;s/$DEFAULT_PREFIX/$PREFIX/g" {} +
+find . -type f -not \( -path './.git/*' -prune \) -not -path './README.md' -exec sed -i '' "s/$BOOTSTRAP_PROJECT_NAME/$PROJECT_NAME/g;s/$BOOTSTRAP_PREFIX/$PREFIX/g" {} +
 
 # The 'Created by' line in the headers of code files
 TODAY=$(date "+%m/%d/%y" | sed 's/^0//g;s/\/0/\//')  # sed nastiness is to remove leading zeroes from the date format
 find . -type f \( -name "*.m" -o -name "*.h" \) -not \( -path './.git/*' -prune \) -exec sed -i '' "s#Created by .* on [0-9].*#Created by $FULLNAME on $TODAY#g" {} +
+
+# Replace default bundle domain and organization
+find . -type f -not \( -path './.git/*' -prune \) -not -path './README.md' -not -path './Podfile' -exec sed -i '' "s/$BOOTSTRAP_ORG/$ORG_NAME/" {} +
+find . -type f -not \( -path './.git/*' -prune \) -not -path './README.md' -not -path './Podfile' -exec sed -i '' "s/$BOOTSTRAP_DOMAIN/$BUNDLE_DOMAIN/" {} +
 
 # Remove ignores and build commands that are only relevant in the bootstrap repo itself
 sed -i '' '/.*>>>bootstrap-only/,/.*<<<bootstrap-only/d' .gitignore .travis.yml
